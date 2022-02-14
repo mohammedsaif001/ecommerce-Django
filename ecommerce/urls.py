98ae1da098a6ac3de1a86184ac0fb00ee56acfb0
Mohammed Saif <mdsaif4online@gmail.com>
2022-02-14 20:36:22 +0530

    1 Adding Bootstrap to the projects and adding static dir

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import shop
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/',include("blog.urls")),
+    path('shop/',include("shop.urls"))
 ]
