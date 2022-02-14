0318ebbe5a8a85709841e7031a1893e2c2ffdcf0
Mohammed Saif <mdsaif4online@gmail.com>
2022-02-14 20:47:13 +0530

    2 Shop - Project Layout

--- shop/views.py
+++ shop/views.py
@@ -5,4 +5,22 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'shop/index.html')
+
+def about(request):
+    return HttpResponse("About")
+
+def contact(request):
+    return HttpResponse("Contact")
+
+def tracker(request):
+    return HttpResponse("Tracker")
+
+def search(request):
+    return HttpResponse("Search")
+
+def productview(request):
+    return HttpResponse("Product View")
+
+def checkout(request):
+    return HttpResponse("CheckOUt")
     
